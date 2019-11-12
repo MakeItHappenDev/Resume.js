9930c5db4a7a63b0b90f2ac6cc8f68f3d07eff03
PookMook <arthur.juchereau@gmail.com>
2019-11-12 15:56:06 -0500

    add few items to the resume.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,11 @@
+let userConfig = {}
+
+userConfig = require('./__now_gatsby_config_user.js')
+
 module.exports = {
-  siteMetadata: {
-    title: `My Resume`,
-    description: `dynamic Resume built from a JSON file`,
-    author: `@arthurBienSur`,
-  },
+  ...userConfig,
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+    ...(userConfig.plugins || []),
+    'gatsby-plugin-now'
+  ]
 }
