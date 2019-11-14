b21cb8183dfd6301d03ba7649dce55d7322f600f
PookMook <arthur.juchereau@gmail.com>
2019-11-14 11:57:27 -0500

    theming

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,11 +1,16 @@
 import React, {useState} from 'react'
 
-export default (props) => {
+export default () => {
 
     const [theme,setTheme] = useState('dark')
 
     let css = `:root{
-    --background:black;
+    --bgColor:#505050;
+    --link: #8ad3ff;
+    --mildHL: #c5ffa4;
+    --color: #c0c0c0;
+    --bgSection: #303030;
+    --lines:#738574;
     }`
 
     if(theme === "dark"){
@@ -13,15 +18,19 @@ export default (props) => {
     else{
         css = `
         :root{
-            --background:white;
-        }
+            --bgColor:#d0d0d0;
+            --link: blue;
+            --mildHL: red;
+            --color: #303030;
+            --bgSection: white;
+            --lines:#FFd0d0;}
         `
     }
     return(
         <>
             <section>
-                <p onClick={()=>setTheme("dark")}>Dark</p>
-                <p onClick={()=>setTheme("light")}>light</p>
+                <p onClick={()=>setTheme("dark")}>Dark Theme</p>
+                <p onClick={()=>setTheme("light")}>light Theme</p>
             </section>
             <style dangerouslySetInnerHTML={{__html: css}} />
         </>
