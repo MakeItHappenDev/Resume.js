d758dfe21b98794b6d88706e59e06610825308aa
PookMook <arthur.juchereau@gmail.com>
2019-12-06 09:56:14 -0500

    add pdf download

--- src/components/meta.js
+++ src/components/meta.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { FaPhone, FaEnvelopeOpen, FaMapMarkerAlt, FaGlobe} from 'react-icons/fa'
+import { FaPhone, FaEnvelopeOpen, FaMapMarkerAlt, FaGlobe, FaFilePdf} from 'react-icons/fa'
+
+import pdf from '../static/resume.pdf'
 
 export default (props) => {
     return(
@@ -9,6 +11,7 @@ export default (props) => {
             {props.meta.contact && <p><FaEnvelopeOpen/> {props.meta.contact}</p>}
             {props.meta.website && <p><FaGlobe/> {props.meta.website}</p>}
             {props.meta.phone && <p><FaPhone/> {props.meta.phone}</p>}
+            {pdf && <p><FaFilePdf /> <a href={pdf} target="_blank">download .pdf</a></p>}
         </section>
     )
 }
